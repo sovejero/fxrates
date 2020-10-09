87d0224df8974720056fc5a093b03a1e951f1666
Santiago Ovejero <santiagodovejero@gmail.com>
2020-10-09 04:23:22 -0300

    style: enforce eslint rules

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-import { 
-    removeRates,
-    showAlert,
-    removeAlert,
-    readBase,
-    readDate,
-    updateRatesTitle,
-    setBaseOptions,
-    removeBaseOptions,
-    populateRatesBody,
-    setDate
+import {
+  removeRates,
+  showAlert,
+  removeAlert,
+  readBase,
+  readDate,
+  updateRatesTitle,
+  setBaseOptions,
+  removeBaseOptions,
+  populateRatesBody,
+  setDate,
 } from './ui.js';
 
-function setUp(){
-    const $update = document.querySelector('#update');
-    $update.onclick = updateRates;
+function setUp() {
+  const $update = document.querySelector('#update');
+  $update.onclick = updateRates;
 
-    getFxRates();
-    setDate();
+  getFxRates();
+  setDate();
 }
 
-function updateRates(){
-    removeRates();
-    const base = readBase();
-    const date = readDate();
-    removeBaseOptions();
-    getFxRates(base, date);
+function updateRates() {
+  removeRates();
+  const base = readBase();
+  const date = readDate();
+  removeBaseOptions();
+  getFxRates(base, date);
 }
 
-function getFxRates(base = 'EUR', date = 'latest'){
-    const URL = `https://api.exchangeratesapi.io/${date}?base=${base}`;
-    fetch(URL)
-        .then( response => response.json() )
-        .then( response => showRates(response) )
-        .catch( error => showAlert(error) );
+function getFxRates(base = 'EUR', date = 'latest') {
+  const URL = `https://api.exchangeratesapi.io/${date}?base=${base}`;
+  fetch(URL)
+    .then((response) => response.json())
+    .then((response) => showRates(response))
+    .catch((error) => showAlert(error));
 }
 
-function showRates(response){
-    removeAlert();
-    setBaseOptions(response);
-    populateRatesBody(response.rates);
-    updateRatesTitle(response.base, response.date)
+function showRates(response) {
+  removeAlert();
+  setBaseOptions(response);
+  populateRatesBody(response.rates);
+  updateRatesTitle(response.base, response.date);
 }
 
 setUp();
